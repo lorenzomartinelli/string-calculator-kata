158262ca61b09d5a5475ece2c660f670aae9e5ff
lorenzomartinelli <lollobop@gmail.com>
2015-11-26 22:05:52 +0100

    Predisposto caso di test che va a verificare Step 2: esito positivo

--- src/test/java/org/agnese/string/CalculatorTest.java
+++ src/test/java/org/agnese/string/CalculatorTest.java
@@ -13,7 +13,8 @@ public class CalculatorTest {
 	private ICalculator calculator = new Calculator();
 
     /**
-     * Esecuzione dei casi di test positivi: stringhe nulle.
+     * Esecuzione dei casi di test positivi: stringhe nulle.</br>
+     * Il test verifica <b>Step 1</b>
      */
     @Test
     public void testZero() {
@@ -23,12 +24,25 @@ public class CalculatorTest {
     }
     
     /**
-     * Esecuzione dei casi di test positivi: somme di numeri
+     * Esecuzione dei casi di test positivi: somme di uno o due numeri</br>
+     * Il test verifica <b>Step 1</b>
      */
     @Test
     public void testSum() {
     	assertTrue( calculator.add("1") == 1 );
+    	assertTrue( calculator.add("157") == 157 );
         assertTrue( calculator.add("1,2") == 3 );
         assertTrue( calculator.add("18,29") == 47 );
+    }
+    
+    /**
+     * Esecuzione dei casi di test positivi: somme di numeri</br>
+     * Il test verifica <b>Step 1</b>
+     */
+    @Test
+    public void testSumUnknownAmountOfNumbers() {
+    	assertTrue( calculator.add("1,2,3,4,5") == 15 );
+        assertTrue( calculator.add("1,3,17,99") == 120 );
+        assertTrue( calculator.add("2,1,18,29,1000,3,3447") == 4500 );
     }
 }
