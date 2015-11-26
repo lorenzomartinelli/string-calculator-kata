fea9cf0cb00a99a6cdaa371900807c3618e2c0d3
lorenzomartinelli <lollobop@gmail.com>
2015-11-26 21:56:44 +0100

    refactor documentazione prima di iniziare lo step 2

--- src/main/java/org/agnese/string/Calculator.java
+++ src/main/java/org/agnese/string/Calculator.java
@@ -1,12 +1,34 @@
 package org.agnese.string;
 
+/**
+ * Implementazione dell'interfaccia {@link ICalculator}.</br>
+ * Ha la responsabilit&agrave; di realizzare un calcolatore che interpreta delle stringhe
+ * come addendi e ritorna il risultato dell'addizione.</br>
+ * Per le esigenze attuali il livello di visibilit&agrave; dell'intefaccia sufficiente
+ * per testarla &egrave; a livello di package.
+ * 
+ * @author Lorenzo Martinelli
+ */
 class Calculator implements ICalculator {
 
+	/**
+	 * Separatori supportati da {@link Calculator}
+	 */
+	private static final String SEPARATOR = ",";
+
+	/**
+	 * Implementazione del metodo add come da specifiche
+	 * Per ora non sono gestite stringhe che non abbiano <code>,</code>
+	 * come separatore
+	 */
 	public int add(String numbers) {
+		// Verifica dello scenario stringa "nulla"
 		if (numbers == null || numbers.trim().isEmpty()) {
 			return 0;
 		}
-		String[] addends = numbers.split(",");
+		
+		// Somma della stringa
+		String[] addends = numbers.split(SEPARATOR);
 		int result = 0;
 		for (int i = 0; i < addends.length; i++) {
 			result = result + Integer.parseInt(addends[i]);
--- src/main/java/org/agnese/string/ICalculator.java
+++ src/main/java/org/agnese/string/ICalculator.java
@@ -1,13 +1,14 @@
 package org.agnese.string;
 
 /**
- * String calculator
+ * Interfaccia che definisce i metodi previsti dal Kata 
+ * <strong>String Calculator Kata</strong>
  * @author Lorenzo Martinelli
  */
 public interface ICalculator {
 	/**
-	 * Il metodo permette di sommare fino a due numeri.
-	 * Se la stringa &egrave; vuota il risultato ottenuto sar&agrave; 0.
+	 * Il metodo permette di sommare fino a due numeri separati da ,.</br>
+	 * Se la stringa &egrave; vuota il risultato ottenuto &egrave; 0.
 	 * @param numbers lista di numeri da sommare
 	 * @return la somma dei numeri
 	 */
